41a5dafe205fa61a45a5459c467f124bcdc787fa
khusniddin007 <xxusanboyev323@gmail.com>
2023-06-05 15:55:26 +0500

    mapper has been created

--- src/main/java/com/comp_accessory/computeraccessories/dto/ProductDTO.java
+++ src/main/java/com/comp_accessory/computeraccessories/dto/ProductDTO.java
@@ -1,12 +1,21 @@
 package com.comp_accessory.computeraccessories.dto;
 
 import com.comp_accessory.computeraccessories.entity.Category;
+import com.comp_accessory.computeraccessories.entity.Product;
 import com.comp_accessory.computeraccessories.entity.ProductExtraDetails;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDTO {
+    private Long id;
     private String serialNumber;
     private String manufacturer;
     private double price;
@@ -14,5 +23,15 @@ public class ProductDTO {
     private Category category;
     private List<ProductExtraDetails> productExtraDetails;
 
-    public static
+    public static ProductDTO mapToProductDTO(Product product){
+        return ProductDTO.builder()
+                .id(product.getId())
+                .serialNumber(product.getSerialNumber())
+                .manufacturer(product.getManufacturer())
+                .price(product.getPrice())
+                .quantity(product.getQuantity())
+                .category(product.getCategory())
+                .productExtraDetails(product.getProductExtraDetails())
+                .build();
+    }
 }
