3b086e8b4941dd6060d58695d4b214d4a8eaca9a
Asrorjon <turgunboyev7212@gmail.com>
2023-06-06 14:15:14 +0500

    Product detail save

--- src/test/java/com/comp_accessory/computeraccessories/controller/ProductControllerTest.java
+++ src/test/java/com/comp_accessory/computeraccessories/controller/ProductControllerTest.java
@@ -1,24 +1,5 @@
 package com.comp_accessory.computeraccessories.controller;
 
-import com.comp_accessory.computeraccessories.dto.ProductExtraDTO;
-import com.comp_accessory.computeraccessories.entity.ProductExtraDetails;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import com.comp_accessory.computeraccessories.dto.ProductDTO;
-import com.comp_accessory.computeraccessories.entity.ProductTypeEnum;
-import com.comp_accessory.computeraccessories.entity.Product;
-import com.comp_accessory.computeraccessories.repository.ProductRepository;
-import com.comp_accessory.computeraccessories.service.ProductServiceImpl;
-import com.comp_accessory.computeraccessories.util.ProductRequest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
 
 class ProductControllerTest {
 
